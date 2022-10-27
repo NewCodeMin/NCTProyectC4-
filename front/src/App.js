b80f0203733f82a3150b24f9fc317f199e111f58
Luis Alfonsop Giraldo Castro <lagc1964@yahoo.com>
2022-10-26 23:51:22 -0500

    chore: modificación de Header, creación de Interfaz Ingresar Producto

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
+import './styles.css';
 import React from 'react';
 import Header from './components/layout/Header';
+/*import Detalleproduadmin from './components/Detalleproduadmin';
+import Listaproadmin from './components/Listaproadmin';
+import CarritoUsuario from './components/CarritoUsuario';
+import Listaventaadmin from './components/Listaventaadmin';
+import Detallesdecompra from './components/Detalles_de_compra';
+import VistaClientes from './components/VistaCliente';*/
+
+import IngresarProducto from './components/IngresarProductos';
+//Router traido desde react-router-dom (no confundir con el de express)
+import { BrowserRouter as Router,Route , Routes } from 'react-router-dom';
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header />
+        <Header />
+        <div className='container container-fluid'>
+          <Routes>
+        
+          <Route path="/IngresarProducto" element={<IngresarProducto />}/>
+          </Routes>
+        </div>
+      
     </div>
+    </Router>
   );
 }
 
--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -3,48 +3,60 @@ import React, { Fragment } from 'react'
 const Header = () => {
     return (
         <Fragment>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynav"
-                        aria-controls="mynav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <a class="navbar-brand" href=" ">
-                        <div class="d-flex">
-                            <img src="./images/logo.jpg" alt="" class="logoNav"></img>
-
-                            <div class="ms-3 d-flex flex-column">
-                                <div class="h4">Tienda de zapatos</div>
-                                <div class="fs-6">New code team</div>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="collapse navbar-collapse" id="mynav">
-                        <table>
-                            <tr>
-                                <td><input type="search" id="form1" class="seacrhNav" placeholder='Buscar' /></td>
-                                <td><button type="button" class="btn btn-searchNav">
-                                <i class="fas fa-search"></i>
-                            </button></td>
-                            </tr>
-                        </table>
-                        <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href=" ">
-                                    <div class="cart bg-purple">
-                                        <span class="fas fa-shopping-cart text-white"></span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href=" "> <span class="fas fa-user pe-2"></span> Hello Jhon</a>
-                            </li>
-                        </ul>
+            <nav class="navbar navbar-dark bg-white ">
+        <div class="container-fluid">
+            <img src="./images/bannert.png "  alt="logo" id = "imagenavbar"/>  
+          <form class="d-flex " id="buscador" role="search">
+            <input class="form-control me-2 id " id= "buscarnav" type="search" placeholder="¿Qué buscas?" aria-label="Search"/>
+            <button class="btn btn-success "  id= "buscarbotom" type="submit">Buscar</button>
+          </form>
+          <div class="navbar-brand  d-md-flex ">
+            <div class="final"> 
+            <div class="d-flex flex-row-reverse">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+            <a class="nav-link" href=" "> <span class="fa fa-shopping-cart"  id="cart_shopping"></span> <span className="ml-0" id="cart_count">0</span> </a>
+           </div>
+           </div>
+           </div>
+          <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Tienda de zapatos deportivos</h5>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <div class="navtobbler"> 
+                        <div class="d-flex ">
+                        <button  type="button" href=" " id="botoningresar"> <span class="fas fa-user pe-2   "></span>Ingresar</button>
+                        <button  type="button"  href=" " id="botoningresar"> <span class="fa fa-user-plus "></span> Crear cuenta</button>
                     </div>
-                </div>
-            </nav>
+                    </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href=" ">Mujer</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href=" ">Hombre</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href=" ">Niñas</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href=" ">Niños</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href=" ">Marcas</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
         </Fragment>
-    )
+    )               
 }
 
 export default Header
