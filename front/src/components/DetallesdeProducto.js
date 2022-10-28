893b27d0355f788c36bd7129c6c6f200439c7c18
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-10-28 05:50:23 -0500

    chore: json consumido para spring

--- front/src/components/DetallesdeProducto.js
+++ front/src/components/DetallesdeProducto.js
@@ -11,7 +11,25 @@ export const DetallesdeProducto = () => {
   const dispatch = useDispatch();
   const alert = useAlert();
   const [quantity, setQuantity] = useState(1)
-
+  const consumir =  {
+    "_id": "635b90b93ee13359fb6c5a78",
+    "nombre": "ZAPATO CASUAL JARRET",
+    "marca": "Adidas",
+    "referencia": "Samba",
+    "genero": "Hombre",
+    "talla": 39,
+    "imagen": [
+        {
+            "public_id": "productos/dsvbpny402gelwugv2le",
+            "url": "./images/productos/Calzado1.jpg",
+            "_id": "635b90b93ee13359fb6c5a79"
+        }
+    ],
+    "inventario": 50,
+    "precio": 195000,
+    "caracteristicas": "color negro, de cuero, y suela eva",
+    "__v": 0
+};
 
   useEffect(() => {
     dispatch(getProductDetails(id))
@@ -43,7 +61,7 @@ export const DetallesdeProducto = () => {
     <Fragment>
       {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> : (
         <Fragment>
-          <MetaData title={product.nombre}></MetaData>
+          <MetaData title={consumir.nombre}></MetaData>
           <section>
             <div class="container py-5">
               <div class="row">
@@ -52,12 +70,12 @@ export const DetallesdeProducto = () => {
                   <ul class="list-group shadow">
 
                     <li class="list-group-item">
-                      <h3 class="mt-0 mb-1">{product.nombre}</h3>
+                      <h3 class="mt-0 mb-1">{consumir.nombre}</h3>
                       <div class="media align-items-lg-center flex-column flex-lg-row">
 
-                        {product.imagen && product.imagen.map(img => (
+                        {consumir.imagen && consumir.imagen.map(img => (
                           <div key={img.public_id}>
-                            <img src={"../" + img.url} alt={product.nombre} width="250" height="200" class="order-1 order-lg-1"></img>
+                            <img src={"../" + img.url} alt={consumir.nombre} width="250" height="200" class="order-1 order-lg-1"></img>
                           </div>
                         ))}
 
@@ -73,43 +91,43 @@ export const DetallesdeProducto = () => {
                               <div class="form-group row">
                                 <label for="inputtext" class="col-sm-2 col-form-label">Marca:</label>
                                 <div class="col-sm-10">
-                                  <label for="inputtext" class="col-sm-2 col-form-label">{product.marca}</label>
+                                  <label for="inputtext" class="col-sm-2 col-form-label">{consumir.marca}</label>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputtext" class="col-sm-2 col-form-label">Referencia:</label>
                                 <div class="col-sm-10">
-                                  <label for="inputtext" class="col-sm-2 col-form-label">{product.referencia}</label>
+                                  <label for="inputtext" class="col-sm-2 col-form-label">{consumir.referencia}</label>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputtext" class="col-sm-2 col-form-label">Genero:</label>
                                 <div class="col-sm-10">
-                                  <label for="inputtext" class="col-sm-2 col-form-label">{product.genero}</label>
+                                  <label for="inputtext" class="col-sm-2 col-form-label">{consumir.genero}</label>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputtext" class="col-sm-2 col-form-label">Talla:</label>
                                 <div class="col-sm-10">
-                                  <label for="inputtext" class="col-sm-2 col-form-label">{product.talla}</label>
+                                  <label for="inputtext" class="col-sm-2 col-form-label">{consumir.talla}</label>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputPassword3" class="col-sm-3 col-form-label">Descripcion:</label>
                                 <div class="col-sm-8">
-                                  <p class="col-form-label">{product.caracteristicas}</p>
+                                  <p class="col-form-label">{consumir.caracteristicas}</p>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputtext" class="col-sm-2 col-form-label">Precio:</label>
                                 <div class="col-sm-10">
-                                  <label for="inputtext" class="col-sm-2 col-form-label">${product.precio}</label>
+                                  <label for="inputtext" class="col-sm-2 col-form-label">${consumir.precio}</label>
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <label for="inputPassword3" class="col-sm-3 col-form-label">Unidades:</label>
                                 <div class="col-sm-8">
-                                  <p class="col-form-label">{product.inventario} unidades</p>
+                                  <p class="col-form-label">{consumir.inventario} unidades</p>
                                 </div>
                               </div>
                               <div className="stockCounter d-inline">
@@ -117,7 +135,7 @@ export const DetallesdeProducto = () => {
                                 <input type="number" className="countlab count d-inline"  value={quantity} readOnly />
                                 <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                               </div>
-                              <button class="btn btn-danger" id="carrito_btn" type="button" disabled={product.inventario === 0}>Agregar al Carrito</button>
+                              <button class="btn btn-danger" id="carrito_btn" type="button" disabled={consumir.inventario === 0}>Agregar al Carrito</button>
                               <div class="mt-3"><button class="btn btn-dark mr-2" type="button">ATRAS</button>
                               </div>
                             </ul>
