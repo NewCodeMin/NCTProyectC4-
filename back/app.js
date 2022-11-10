733a088a1990de3b62e357231d681b49140328ca
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-11-10 01:39:09 -0500

    chore: Registro del usuario (modelo, controlador y ruta)

--- back/app.js
+++ back/app.js
@@ -6,13 +6,15 @@ app.use(express.json());
 
 //Importar rutas
 const productos= require("./routes/products")
+const usuarios=require("./routes/auth")
 
 app.use('/api',productos) //Sujeto a decision (ruta del navegador)
+app.use('/api',usuarios)
 
+//MiddleWares para manejar errores
+app.use(errorMiddleware)
 
 //MiddleWares para manejar errores
 app.use(errorMiddleware)
 
-//MiddleWares para manejar errores
-app.use(errorMiddleware)
 module.exports=app
