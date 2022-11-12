11fc82a599177086e67355733dfb64c687f391e2
Luis Alfonsop Giraldo Castro <lagc1964@yahoo.com>
2022-11-12 03:13:54 -0500

    chore: Se realiza actualización de inventario por orden de venta

--- back/controllers/orderController.js
+++ back/controllers/orderController.js
@@ -1,6 +1,7 @@
 const Order=require("../models/order");
 const catchAsyncErrors= require("../middleware/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
+const producto=require("../models/productos");
 
 //Crear una nueva orden
 exports.newOrder= catchAsyncErrors (async (req, res, next)=>{
@@ -54,4 +55,34 @@ exports.myOrders= catchAsyncErrors(async(req,res, next)=>{
         success:true,
         orders
     })
+})
+//Finalizar orden
+exports.finishOrder = catchAsyncErrors(async(req,res,next) => {
+    let order= await Order.findById(req.params.id).populate("user", "nombre email") //restriccion de usuario
+
+    if(!order){
+        return next(new ErrorHandler("No encontramos una orden con ese Id",404))
+    }
+   //Si el objeto existe
+   order = await  Order.findByIdAndUpdate(req.params.id)
+    order.estado = "Prueba"
+    let itemsProd = [];
+    itemsProd = order.items;
+    let idProducts = [];
+    let listCantidad = [];
+    for (i = 0 ; i < itemsProd.length;i++){
+        idProducts.push(itemsProd[i].producto);
+        listCantidad.push(itemsProd[i].cantidad);
+    }
+    for (i = 0 ; i < idProducts.length;i++){
+        let product = await producto.findById(idProducts.slice(i,i+1));
+        product.inventario -= listCantidad[i];
+        product = await producto.findByIdAndUpdate(idProducts.slice(i,i+1),product);
+    }
+        res.status(200).json({
+        success: true,
+        message: "actualizacion correcta", 
+        order
+    })
+
 })
--- back/routes/orders.js
+++ back/routes/orders.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const { newOrder,
     getOneOrder,
     myOrders, 
-
+    finishOrder,
 
 } = require("../controllers/orderController");
 const { isAuthenticatedUser } = require("../middleware/auth");
@@ -11,5 +11,6 @@ const { isAuthenticatedUser } = require("../middleware/auth");
 router.route("/order/new").post(isAuthenticatedUser, newOrder)
 router.route("/order/:id").get(isAuthenticatedUser, getOneOrder)
 router.route("/orders/me").get(isAuthenticatedUser, myOrders)
+router.route("/orderFinish/:id").put(isAuthenticatedUser, finishOrder)
 
 module.exports=router;
